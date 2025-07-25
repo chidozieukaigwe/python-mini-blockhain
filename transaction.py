b5ff32343f1575b59b9f8efe0f8dd98842a20f27
chido <chidodesigns@gmail.com>
2025-07-25 23:39:11 +0100

    Add verify transaction logic to wallet class

--- transaction.py
+++ transaction.py
@@ -2,10 +2,19 @@ from collections import OrderedDict
 from utility.printable import Printable
 
 class Transaction(Printable):
-    def __init__(self, sender, recipient, amount ):
+    """A transaction which can be added to a block in the blockchain
+
+    Attributes:
+        :sender: The sender of the transaction
+        :recipient: The recipient of the transaction
+        :signature: The signature of the transaction
+        :amount: The amount of the transaction
+    """
+    def __init__(self, sender, recipient, signature, amount ):
         self.sender = sender
         self.recipient = recipient
         self.amount = amount
+        self.signature = signature
 
     def to_ordered_dict(self):
         return OrderedDict([('sender', self.sender), ('recipient', self.recipient),('amount', self.amount),])
--- utility/verification.py
+++ utility/verification.py
@@ -2,6 +2,7 @@
 
 from utility.hash_util import hash_string_256, hash_block
 from transaction import Transaction
+from wallet import Wallet
 class Verification:
 
     @classmethod
@@ -22,9 +23,12 @@ class Verification:
         return True
 
     @staticmethod
-    def verify_transaction(transaction: Transaction, get_balance):
-        sender_balance = get_balance()
-        return sender_balance >= transaction.amount
+    def verify_transaction(transaction: Transaction, get_balance, check_funds=True):
+        if check_funds:
+            sender_balance = get_balance()
+            return sender_balance >= transaction.amount and Wallet.verify_transaction(transaction)
+        else:
+            return Wallet.verify_transaction(transaction)
 
     @classmethod
     def verify_transactions(cls, open_transactions, get_balance) -> bool:
@@ -32,8 +36,7 @@ class Verification:
         Verifies all open transactions
         :return: bool
         """
-        # Check all transactions in one go via the all function and list comprehension
-        return all([cls.verify_transaction(tx, get_balance) for tx in open_transactions])
+        return all([cls.verify_transaction(tx, get_balance, False) for tx in open_transactions])
 
     @staticmethod
     def valid_proof(transactions: list, last_hash: str, proof: int) -> bool:
