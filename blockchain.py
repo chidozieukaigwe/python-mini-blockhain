9d02eb8974b7697846e3cca3df50db24ca62e40a
chido <chidodesigns@gmail.com>
2025-07-26 11:36:41 +0100

    Add Node Management methods to the Blockchain

--- blockchain.py
+++ blockchain.py
@@ -22,11 +22,14 @@ class Blockchain:
         self.__open_transactions = []
         self.load_data()
         self.hosting_node = hosting_node_id
+        self.__peer_nodes = set()
 
+    # Decorator acts as a get to the property
     @property
     def chain(self) -> list[Block]:
         # Return a copy of list not reference
         return self.__chain[:]
+
     @chain.setter
     def chain(self, value: list[Block]) -> None:
         self.__chain = value
@@ -63,12 +66,14 @@ class Blockchain:
                     updated_blockchain.append(updated_block)
 
                 self.chain = updated_blockchain
-                open_transactions = json.loads(file_content[1])
+                open_transactions = json.loads(file_content[1][:-1])
                 updated_transactions = []
                 for tx in open_transactions:
                     updated_transaction = Transaction(tx['sender'], tx['recipient'], tx['signature'], tx['amount'])
                     updated_transactions.append(updated_transaction)
                 self.__open_transactions = updated_transactions
+                peer_nodes = json.loads(file_content[2])
+                self.__peer_nodes = set(peer_nodes)
         except (IOError, IndexError):
                pass
         finally:
@@ -84,6 +89,8 @@ class Blockchain:
                 file.write('\n')
                 saveable_tx = [tx.__dict__ for tx in self.__open_transactions]
                 file.write(json.dumps(saveable_tx))
+                file.write('\n')
+                file.write(json.dumps(list(self.__peer_nodes)))
 
                 # Integrate with Pickle
                 # save_data = {
@@ -190,6 +197,20 @@ class Blockchain:
         self.save_data()
         return block
 
+    def add_peer_node(self, node):
+        """ Add a new node to the peer node set
 
+            Arguments:
+                :node: The node URL which be added
+        """
+        self.__peer_nodes.add(node)
+        self.save_data()
 
+    def remove_peer_node(self, node):
+        """ Remove a node from the peer node set
 
+        Arguments:
+              :node: The node URL which will be removed
+        """
+        self.__peer_nodes.discard(node)
+        self.save_data()
